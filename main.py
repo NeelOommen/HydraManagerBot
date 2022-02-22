c063cc2cc351bca85ec524d5fb7e0f64c305cdaa
Tron51226 <70756241+Tron51226@users.noreply.github.com>
2022-02-22 15:05:52 +0530

    Added files from Replit
    Added the bot code that I originally wrote in Replit to the repository

---
+++ main.py
@@ -0,0 +1,25 @@
+import discord
+import os 
+#from keep_online import keep_online
+
+bot_token = os.environ['TOKEN']
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+  print('Bot logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+    
+  #547905866255433758 is Hydra's user id
+  if message.author.id == 547905866255433758:
+    await message.delete()
+    
+  if message.content.startswith("."):
+    await message.delete()
+
+client.run(bot_token)
